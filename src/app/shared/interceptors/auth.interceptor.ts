eb041130cd1da89ee97254402420be25bd1beaac
Seba <52335419+cgozu@users.noreply.github.com>
2025-07-10 19:55:10 -0500

    Add proxy config and improve auth interceptor

--- src/app/shared/interceptors/auth.interceptor.ts
+++ src/app/shared/interceptors/auth.interceptor.ts
@@ -2,13 +2,18 @@ import { HttpInterceptorFn } from '@angular/common/http';
 
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
   const token = localStorage.getItem('token');
+  
+  let authReq = req.clone({
+    headers: req.headers
+      .set('Content-Type', 'application/json')
+      .set('Accept', 'application/json')
+  });
 
   if (token) {
-    const authReq = req.clone({
-      headers: req.headers.set('Authorization', `Bearer ${token}`)
+    authReq = authReq.clone({
+      headers: authReq.headers.set('Authorization', `Bearer ${token}`)
     });
-    return next(authReq);
   }
 
-  return next(req);
+  return next(authReq);
 };
